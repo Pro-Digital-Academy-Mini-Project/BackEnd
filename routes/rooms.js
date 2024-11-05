17d0dc0f80a80c699648faa6b247dc67051bf9f1
kwak <sal091625@gmail.com>
2024-11-05 14:26:21 +0900

    feat: room getById로 room, video data 가져오기

--- routes/rooms.js
+++ routes/rooms.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const Room = require('../models/Room');
+const Video = require('../models/Video'); // Video 모델 경로
 
 router.get('/', (req, res, next) => {
   Room.find().then(rooms => {
@@ -11,12 +12,11 @@ router.get('/', (req, res, next) => {
   })
 });
 
-router.get('/:id', (req, res, next)=>{
+
+// 특정 Room의 상세 정보와 관련된 Video 정보 가져오기
+router.get('/:id', (req, res, next) => {
   Room.findById(req.params.id)
-    .populate('video_id') // Video 정보 가져오기
-    .populate('participants') // 참가자 정보 가져오기
-    .populate('comments') // 댓글 작성자 정보 가져오기
-    .populate('timeline_comments') // 타임라인 댓글 작성자 정보 가져오기
+    .populate('video_id') // video_id 참조 필드를 Video 정보로 채우기
     .then(room => {
       if (!room) {
         return res.status(404).json({ message: 'Room not found' });
@@ -26,8 +26,7 @@ router.get('/:id', (req, res, next)=>{
     .catch(err => {
       next(err);
     });
-})
-
+});
 router.post('/', (req, res, next) => {
   console.log(req.body);
   Room.create(req.body).then(room => {
