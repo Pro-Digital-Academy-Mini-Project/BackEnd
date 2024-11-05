2964c9f5bae5f0bcceb008f6bf76167ae5eb3dda
kwak <sal091625@gmail.com>
2024-11-05 14:16:03 +0900

    fix: get rooms

--- routes/rooms.js
+++ routes/rooms.js
@@ -11,6 +11,23 @@ router.get('/', (req, res, next) => {
   })
 });
 
+router.get('/:id', (req, res, next)=>{
+  Room.findById(req.params.id)
+    .populate('video_id') // Video 정보 가져오기
+    .populate('participants') // 참가자 정보 가져오기
+    .populate('comments') // 댓글 작성자 정보 가져오기
+    .populate('timeline_comments') // 타임라인 댓글 작성자 정보 가져오기
+    .then(room => {
+      if (!room) {
+        return res.status(404).json({ message: 'Room not found' });
+      }
+      res.json(room);
+    })
+    .catch(err => {
+      next(err);
+    });
+})
+
 router.post('/', (req, res, next) => {
   console.log(req.body);
   Room.create(req.body).then(room => {
