e0648f90e8e47858b5288bceafe34ecea4cd6e15
ham <tomy8964@naver.com>
2024-11-07 14:42:37 +0900

    TimeLine Schema username 추가 및 timelineComment username 추가

--- routes/timelinecomments.js
+++ routes/timelinecomments.js
@@ -3,32 +3,21 @@ const router = express.Router();
 
 const TimeLineComment = require("../models/TimeLineComment");
 
-router.get("/", (req, res, next) => {
-  TimeLineComment.findByRoomId()
-    .then((timeLineComments) => {
-      res.cookie("my-cookie", "cookie-value", {
-        maxAge: 1000 * 60 * 60 * 24,
-        secure: false,
-        httpOnly: true,
-        signed: false,
-      });
-      res.json(timeLineComments);
-    })
-    .catch((err) => {
-      next(err);
-    });
-});
-
-router.post("/", (req, res, next) => {
+router.post("/", async (req, res, next) => {
   console.log(req);
-  // token -> user.email -> user._id 로 저장
+  const commentUser = req.body(usernamse);
+  const user = await User.findOne({ username: commentUser });
+  if (!user) {
+    return res.status(404).json({ message: "Owner not found" });
+  }
   const comment = {
-    user_id: "672871a44f2b2bf03d7c9192",
+    user_id: user.user_id,
+    username: user.username,
     room_id: req.body.room_id,
     timestamp: req.body.timestamp,
     content: req.body.content,
   };
-  
+
   TimeLineComment.create(comment)
     .then((timeLineComment) => {
       res.json(timeLineComment);
@@ -45,15 +34,15 @@ router.get("/:roomId", async (req, res, next) => {
     });
 
     const returnComments = timeLineComments.map((comment) => {
-        return {
-          // jwt 토큰을 변환해서 username 가져오기
-          username: comment.user_id,
-          content: comment.content,
-          room_id: comment.room_id,
-          timestamp: comment.timestamp,
-          created_at: comment.created_at
-        }
-    })
+      return {
+        // jwt 토큰을 변환해서 username 가져오기
+        username: comment.username,
+        content: comment.content,
+        room_id: comment.room_id,
+        timestamp: comment.timestamp,
+        created_at: comment.created_at,
+      };
+    });
     res.json(returnComments);
   } catch (err) {
     next(err);
