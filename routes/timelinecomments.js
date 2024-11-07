6b68176f7c71ac4fd7da501bb76afc399c9e6359
ham <tomy8964@naver.com>
2024-11-07 18:49:24 +0900

    username 으로 변경하여 저장

--- routes/timelinecomments.js
+++ routes/timelinecomments.js
@@ -2,16 +2,32 @@ const express = require("express");
 const router = express.Router();
 
 const TimeLineComment = require("../models/TimeLineComment");
+const User = require("../models/User");
+
+router.get("/", (req, res, next) => {
+  TimeLineComment.findByRoomId()
+    .then((timeLineComments) => {
+      res.cookie("my-cookie", "cookie-value", {
+        maxAge: 1000 * 60 * 60 * 24,
+        secure: false,
+        httpOnly: true,
+        signed: false,
+      });
+      res.json(timeLineComments);
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
 
 router.post("/", async (req, res, next) => {
-  console.log(req);
-  const commentUser = req.body(usernamse);
+  const commentUser = req.body.username;
   const user = await User.findOne({ username: commentUser });
   if (!user) {
     return res.status(404).json({ message: "Owner not found" });
   }
   const comment = {
-    user_id: user.user_id,
+    user_id: user._id,
     username: user.username,
     room_id: req.body.room_id,
     timestamp: req.body.timestamp,
@@ -35,7 +51,6 @@ router.get("/:roomId", async (req, res, next) => {
 
     const returnComments = timeLineComments.map((comment) => {
       return {
-        // jwt 토큰을 변환해서 username 가져오기
         username: comment.username,
         content: comment.content,
         room_id: comment.room_id,
